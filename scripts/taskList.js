d858d04e5e90918aaf482f288b74fceed23433cd
Hedim Ramirez <ody360@gmail.com>
2018-07-22 19:27:28 -0700

    Got center div to show lists, but still working on left div lists

--- scripts/taskList.js
+++ scripts/taskList.js
@@ -8,24 +8,91 @@ function getTasks(token) {
     }
   })
   .then(res => {
-    console.log(res)
+    console.log('THIS', res.data)
+    
+ //   document.querySelector('#toggleTasks').classList.toggle('d-none', false)
     const left = document.querySelector('#left')
     left.innerHTML =  taskListTemplate.getAllLists()
     const ul = document.querySelector('#all-lists')
     const lists = res.data.lists
-    console.log(lists[0])
+
+    
+    const userID = lists[0].user_id
+    console.log(userID)
+    window.location.hash = `/login/${userID}`
+
+    //CENTER PANEL
+    const center = document.querySelector('#center')
+    center.innerHTML = taskListTemplate.centerTasks()
+    const doingUL = document.querySelector('#doingUL')
+    
+    
+    
+    
     
     lists.forEach(list => {
+      console.log(list)
+      
       const li = document.createElement('li')
-      li.className = 'list-group-item'
-      li.innerText = list.title
-      ul.appendChild(li)
+        li.className = 'list-group-item'
+        li.innerText = list.title
+        ul.appendChild(li)
+        li.addEventListener('click',() => {
+          console.log('clicked',li.classList)
+          li.classList.add('active')
+        })
+      })
+
+    ul.firstElementChild.classList = 'list-group-item active'
+
+    
+      //FOR CENTER PANEL
+      let tasks = list.tasks
+      console.log('TASKS: ', userID)
+      tasks.forEach(task => {
+        //console.log('task.title:', task.title)
+        const doingLi = document.createElement('li')
+        doingLi.innerHTML += taskListTemplate.doingCards(task.title, task.description)
+        doingUL.appendChild(doingLi)
+       })
     })
+
+    
 
     left.innerHTML += taskListTemplate.newTaskForm()
 
+    // const newTask = document.querySelector("newTask")
+    
 
-  })
+    // newTask.addEventListener('submit', (event) => {
+    //   event.preventDefault()
+
+    //   console.log('IN ADD E LISTENER')
+    //   createTaskList(token, listId)
+   // })
+  }
+
+
+
+function createTaskList(token, listId) {
+  const newTitle = document.querySelector('#title').value
+  const newDesc = document.querySelector('#description').value
+  
+
+ console.log('IN CREATE TASK', token)
+
+  return axios.post(`http://localhost:5000/api/lists/${listId}/tasks/`,
+  {
+    headers: {
+      authorization: `Bearer ${token}` 
+    }
+  }, {title: newTitle, description: newDesc, list_id:3}  //explicit list id but needs to be from selection
+  ).then(res => {
+        console.log(res)
+    })
+
 }
 
-module.exports = { getTasks }
+
+
+module.exports = { getTasks, createTaskList }
